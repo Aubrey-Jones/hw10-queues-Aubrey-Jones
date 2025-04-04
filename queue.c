7e59c2b96629d23b87c91e047586088ff00fac53
Aubrey-Jones <jone2482@purdue.edu>
2025-04-03 22:44:05 -0400

    git commit

--- queue.c
+++ queue.c
@@ -1,6 +1,8 @@
 #include "queue.h"
 #include "tile_game.h"
-
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
 
 void enqueue(struct queue *q, struct game_state state) {
     int spot = serialize(state);
@@ -16,18 +18,26 @@ struct game_state dequeue(struct queue *q) {
 
 int number_of_moves(struct game_state start) {
     struct queue *q;
-    struct list_node value;
-    (q -> data.head) = NULL;
-    while (q->data.head != NULL) {
-        dequeue(q);
-        if (equals(cur, search)) {
-            return cur;
-        } else {
-            for (node child in children(cur)) {
-                enqueue(&q, child);
+    struct list_node* new_node = (struct list_node*)malloc(sizeof(struct list_node));
+    new_node -> next = NULL;
+
+    if (q -> data.head == NULL){
+        q -> data.head = new_node;
+    }
+/*
+    new_node bfs(graph g, node start, node search) {
+        queue q = new_queue();
+        while (!empty(s)) {
+            node cur = dequeue(&q);
+            if (equals(cur, search)) {
+                return cur;
+            } else {
+                for (node child in children(cur)) {
+                    enqueue(&q, child);
+                }
             }
         }
     }
-
+*/
     return 0; 
 }
