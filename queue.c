a21397938be6579b884fb34944e9372d15998f65
Aubrey-Jones <jone2482@purdue.edu>
2025-04-03 22:04:26 -0400

    git commit

--- queue.c
+++ queue.c
@@ -1,19 +1,33 @@
 #include "queue.h"
 #include "tile_game.h"
 
+
 void enqueue(struct queue *q, struct game_state state) {
-    
-    
+    int spot = serialize(state);
+    insert_at_tail((&q -> data),spot);
+
 }
 
 struct game_state dequeue(struct queue *q) {
-    
+    remove_from_head(&q -> data);
     
     return (struct game_state){0}; 
 }
 
 int number_of_moves(struct game_state start) {
-    
+    struct queue *q;
+    struct list_node value;
+    (q -> data.head) = NULL;
+    while (q->data.head != NULL) {
+        dequeue(q);
+        if (equals(cur, search)) {
+            return cur;
+        } else {
+            for (node child in children(cur)) {
+                enqueue(&q, child);
+            }
+        }
+    }
 
     return 0; 
 }
